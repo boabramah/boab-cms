38955a86e84dbf0a00a6ae165bfdb4b7a840d74f
boabramah <boabramah@gmail.com>
2017-08-05 00:53:39 +0000

    deleted public static function getRouteConfiguration

--- src/Invetico/UserBundle/UserBundle.php
+++ src/Invetico/UserBundle/UserBundle.php
@@ -16,14 +16,6 @@ class UserBundle extends Bundle
 		return 'user_bundle';
 	}
 
-	public static function getRouteConfiguration()
-	{
-		return [
-			__DIR__.'/Resources/config/routing/routes.xml',
-			__DIR__.'/Resources/config/routing/account.xml'
-		];
-	}
-
 	public static function getMenuConfiguration()
 	{
 		return __DIR__ .'/Resources/config/menu.php';
