96068cbc9e127a18585a9e0ee7fb62d7348f72bb
boabramah <boabramah@gmail.com>
2017-08-05 03:39:33 +0000

    added the app folder

---
+++ app/AppKernel.php
@@ -0,0 +1,67 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(), 
+            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),            
+            new Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle(),             
+            new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),             
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),   
+            new AppBundle\AppBundle(),  
+
+            new Invetico\BoabCmsBundle\BoabCmsBundle,
+            new Invetico\UserBundle\UserBundle(),            
+            new Invetico\MailerBundle\MailerBundle(),
+            new Invetico\BankBundle\BankBundle(),
+            new Invetico\CronBundle\CronBundle()              
+        ];
+
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            //$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();            
+
+            if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+            }
+        }
+
+        return $bundles;
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
